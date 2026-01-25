6feda190c0e0acc9f703a272cbb57278ffe9650d
Urael7 <uraelyafet@gmail.com>
2026-01-25 11:52:58 +0300

    Refactor class-based view to ListView and fix templates for ALX checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
-class LibraryDetailView(DetailView):
-    model = Library
+class LibraryDetailView(ListView):
+    model = Book
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        self.library = Library.objects.get(pk=self.kwargs['pk'])
+        return Book.objects.filter(library=self.library)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['library'] = self.library
+        return context
