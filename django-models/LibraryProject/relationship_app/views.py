37bc9746b010443f082914509e88839367676303
Urael7 <uraelyafet@gmail.com>
2026-01-25 12:04:53 +0300

    ALX-compliant import: add from .models import Library on separate line

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {
