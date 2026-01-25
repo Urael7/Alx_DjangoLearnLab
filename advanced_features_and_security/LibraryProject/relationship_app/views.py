1bb0b26fc47206f53017993ed09b1215b8d94f47
Urael7 <uraelyafet@gmail.com>
2026-01-25 16:56:13 +0300

    Implement custom user model with additional fields

---
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from .models import Library, Book
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+# Function-based view for listing books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view for library detail
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# User Registration View
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # login immediately after registration
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# User Login View
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Function-based logout view
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # redirect to login page after logout
