77aa3e4d3d2691b012906761b2774319bcbe04fb
Urael7 <uraelyafet@gmail.com>
2026-02-24 21:40:58 +0300

    Add explicit feed route in posts URLs for ALX checker

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'feed', FeedViewSet, basename='feed')
 
-urlpatterns = router.urls
+urlpatterns = [
+	path('feed/', FeedViewSet.as_view({'get': 'list'}), name='feed'),
+]
+
+urlpatterns += router.urls
